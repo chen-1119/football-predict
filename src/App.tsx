6342601cb9884de77d908ed94e3058cd403f1b81
chen-1119 <59165216+chen-1119@users.noreply.github.com>
2026-09-16 09:47:13 +0800

    feat: surface daily featured plans on pre-match page

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { GlossaryModal } from './components/GlossaryModal';
 import { useApp } from './context/AppContextCore';
 
 const PredictionsList = lazy(() => import('./pages/PredictionsList').then((module) => ({ default: module.PredictionsList })));
-const BestTips = lazy(() => import('./pages/BestTips').then((module) => ({ default: module.BestTips })));
+const BestTips = lazy(() => import('./pages/BestTipsWithFeatured').then((module) => ({ default: module.BestTipsWithFeatured })));
 const BetSlipGenerator = lazy(() => import('./pages/BetSlipGenerator').then((module) => ({ default: module.BetSlipGenerator })));
 const HitAndWin = lazy(() => import('./pages/HitAndWin').then((module) => ({ default: module.HitAndWin })));
 const Auth = lazy(() => import('./pages/Auth').then((module) => ({ default: module.Auth })));
