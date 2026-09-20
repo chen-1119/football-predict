9b002ecef3d01e64eb8e9c3ecb951976d8846c1c
chen-1119 <chen-1119@users.noreply.github.com>
2026-09-20 08:04:10 +0000

    deploy: 3ac81019122465f03bab1ab9a0f8b9c385b9139e

--- assets/BestTips-DMMogfZk.js
+++
@@ -1,1 +0,0 @@
-import{b as e,u as t}from"./index-D6-ZOXwl.js";import{t as n}from"./RecommendationCenter-C7QePacn.js";var r=t();function i({onSelectMatch:t}){let{language:i}=e();return(0,r.jsx)(n,{language:i,onSelectMatch:t})}export{i as BestTips};
