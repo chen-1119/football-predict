0ebe5a47623cc6a4caf8b98d9ee4d3600b4349f0
chen-1119 <chen-1119@users.noreply.github.com>
2026-09-22 07:20:59 +0000

    deploy: 5bf7df1d269a22d3c5bddb206d5735580733a6af

--- assets/BetSlipGenerator-CSxvsfZl.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-BF6S49EV.js";import{t}from"./RecommendationCenter-BZ7-IEGu.js";import{k as n}from"./index-Dd54i-F8.js";var r=e();function i({onSelectMatch:e}){let{language:i}=n();return(0,r.jsx)(t,{language:i,initialTab:`two`,onSelectMatch:e})}export{i as BetSlipGenerator};
