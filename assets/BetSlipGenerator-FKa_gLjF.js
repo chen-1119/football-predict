18c793385834b2dc1d216eaa53c127c4b10ca443
chen-1119 <chen-1119@users.noreply.github.com>
2026-09-24 08:21:27 +0000

    deploy: 3942b9a0c923d51659906257c5163ec495e902e5

--- assets/BetSlipGenerator-FKa_gLjF.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-BF6S49EV.js";import{t}from"./RecommendationCenter-Y6xpsUMr.js";import{k as n}from"./index-D5GweN8I.js";var r=e();function i({onSelectMatch:e}){let{language:i}=n();return(0,r.jsx)(t,{language:i,initialTab:`two`,onSelectMatch:e})}export{i as BetSlipGenerator};
