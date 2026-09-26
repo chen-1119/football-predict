604fd493c68125201c26a8ad4f19817778f6a324
chen-1119 <chen-1119@users.noreply.github.com>
2026-09-26 01:26:09 +0000

    deploy: 4aac15089ce0e1e038c0cffc032a7f421ece1ccf

--- assets/BetSlipGenerator-BvdvqlA9.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-BF6S49EV.js";import{t}from"./RecommendationCenter-B3fNPXHO.js";import{k as n}from"./index-DCJbHG-4.js";var r=e();function i({onSelectMatch:e}){let{language:i}=n();return(0,r.jsx)(t,{language:i,initialTab:`two`,onSelectMatch:e})}export{i as BetSlipGenerator};
